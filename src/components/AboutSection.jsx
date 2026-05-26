30a0464d9e84f69e16471f5acdcb1913ee9c6a6b
AakaashSharrma <aakaash22k3@gmail.com>
2026-05-26 23:03:50 +0530

    Fix GitHub Pages asset paths

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
             </p>
 
             <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
-              <a href="/#contact" className="cosmic-button">
+              <a href="#contact" className="cosmic-button">
                 {" "}
                 Get In Touch
               </a>
