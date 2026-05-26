631875e8d2f618370de0b619c019bbeb7eb3f8e1
AakaashSharrma <aakaash22k3@gmail.com>
2026-05-26 16:53:01 +0530

    Fix Vite base config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/AakaashS_Portfolio/',
+  base: process.env.NODE_ENV === "production"
+  ? "/AakaashS_Portfolio/"
+  : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
